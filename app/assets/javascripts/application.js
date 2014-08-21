f178a87b39841030e146544f79c5563dfd53b7d7
stellajkimm <stellajkimm@gmail.com>
2014-08-21 15:17:13 -0400

    jquery for signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+	$('.hidden').hide()
+	$('.show').on('click', function(){
+		$(this).parent().hide()
+		$('.hidden').show()
+	})
+})
